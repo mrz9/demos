02ec56762f1bc99234948bab1d6c46645ad066c3
dayao <dayao2525@gmail.com>
2018-08-31 11:26:31 +0700

    add fillter-select

---
+++ filter-select/filter-select.js
@@ -0,0 +1,107 @@
+/**
+ * 过滤select的重复项，只考虑单选，多选的需要自行扩展
+ * @param {*} name 
+ * @param {*} data {value:'',text:'',...}
+ */
+function filterSelect(name,data){
+    if(!window.filterSelectMap){
+        window.filterSelectMap = {};
+    }
+
+    var fsItem = filterSelectMap[name];
+
+    if(fsItem){
+        return fsItem;
+    }else{
+        fsItem = {};
+        filterSelectMap[name] = fsItem;
+    }
+
+    fsItem.name = name;
+    fsItem.origin = JSON.parse(JSON.stringify(data));
+    //已使用的集合
+    fsItem.used = [];
+
+    //获取为选择的集合
+    fsItem.getSafeList = function(){
+        var arr = [];
+        $.each(fsItem.origin,function(i,s){
+            var str = JSON.stringify(s)
+            if(fsItem.used.indexOf(str) === -1){
+                arr.push(s);
+            }
+        });
+        return arr;
+    }
+
+    //更新select的选项
+    fsItem.update = function(){
+        //每次update需要更新used数组
+        fsItem.used = [];
+        //每个select的options集合
+        var selectHtmls = [];
+        //第一次循环设定默认值
+        $('select[filter-name="'+fsItem.name+'"]').each(function(idx){
+            var self = this;
+            var self_val = $(this).val();
+            var options = '';
+            if(self_val !== '' && self_val !== null){
+                var rs = getItemByValue(self_val);
+                if(rs !== null){
+                    var rs_string = JSON.stringify(rs);
+                    if(fsItem.used.indexOf(rs_string) === -1){
+                        fsItem.used.push(rs_string);
+                    }
+                    options = '<option selected="" value="'+ rs.value +'">'+rs.text+'</option>';
+                }
+            }else{
+                var safeList = fsItem.getSafeList();
+                if(safeList.length>0){
+                    var s = safeList[0];
+                    fsItem.used.push(JSON.stringify(s));
+                    options = '<option selected="" value="'+ s.value +'">'+s.text+'</option>';
+                }
+            }
+            selectHtmls.push(options);
+        });
+
+        //获取为选择的option
+        var safe_html = '';
+        $.each(fsItem.getSafeList(),function(i,s){
+            safe_html += '<option value="'+ s.value +'">'+s.text+'</option>';
+        });
+        //第二次循环补充可选的option
+        $('select[filter-name="'+fsItem.name+'"]').each(function(idx){
+            var html = selectHtmls[idx] + safe_html;
+            $(this).html(html);
+        });
+    }
+
+    //实例化
+    init();
+    /**
+     * 内部方法
+     */
+    //初始化
+    function init(){
+        fsItem.update();
+        //chang事件
+        $('body').on('change','select[filter-name="'+fsItem.name+'"]',function(){
+            fsItem.update();
+        })
+
+    }
+
+    //根据值去的对应的item
+    function getItemByValue(value){
+        var item = null;
+        $.each(fsItem.origin,function(i,s){
+            if(s.value === value){
+                item = s;
+                return false;
+            }
+        });
+
+        return item;
+    }
+}
